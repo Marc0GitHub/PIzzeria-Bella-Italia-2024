72f5c66ac978f19279cc53f9bd9f651f54649275
Marc0GitHub <marco.tancredi178@gmail.com>
2024-07-16 16:59:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navbar = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.navbar a');
+
+mobileMenu.addEventListener('click', () => {
+    if (navbar.classList.contains('active')) {
+        navbar.classList.remove('active');
+        navbar.classList.add('remove-active');
+        setTimeout(() => {
+            navbar.style.display = 'none';
+        }, 500); // Duration of the slide-out animation
+        mobileMenu.classList.remove('active');
+    } else {
+        navbar.style.display = 'flex';
+        setTimeout(() => {
+            navbar.classList.remove('remove-active');
+            navbar.classList.add('active');
+        }, 10); // Slight delay to ensure the display change takes effect
+        mobileMenu.classList.add('active');
+    }
+});
+
+document.addEventListener('click', (e) => {
+    if (navbar.classList.contains('active') && !navbar.contains(e.target) && !mobileMenu.contains(e.target)) {
+        navbar.classList.remove('active');
+        navbar.classList.add('remove-active');
+        setTimeout(() => {
+            navbar.style.display = 'none';
+        }, 500); // Duration of the slide-out animation
+        mobileMenu.classList.remove('active');
+    }
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (navbar.classList.contains('active')) {
+            navbar.classList.remove('active');
+            navbar.classList.add('remove-active');
+            setTimeout(() => {
+                navbar.style.display = 'none';
+            }, 500); // Duration of the slide-out animation
+            mobileMenu.classList.remove('active');
+        }
+    });
+});
+
+window.addEventListener('scroll', reveal);
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+    }
+}

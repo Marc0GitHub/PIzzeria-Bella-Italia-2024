3e1e98f44ce267ff760900bfcd46f244d0f9aa71
Marc0GitHub <marco.tancredi178@gmail.com>
2024-07-23 23:44:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -250,8 +250,7 @@ function clock() {// We create a new Date object and assign it to a variable cal
         // Access the "getMinutes" method with the dot accessor.
         minutes = time.getMinutes();
         
-        
-        
+   
     
     document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes);
       
@@ -264,4 +263,21 @@ function clock() {// We create a new Date object and assign it to a variable cal
     }
     setInterval(clock, 1000);
 
+    document.addEventListener("DOMContentLoaded", function() {
+        const headerHeight = document.querySelector("header").offsetHeight;
+    
+        document.querySelectorAll("a[href^='#']").forEach(anchor => {
+            anchor.addEventListener("click", function(e) {
+                e.preventDefault();
+                const targetId = this.getAttribute("href").substring(1);
+                const targetElement = document.getElementById(targetId);
+                const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+    
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: "smooth"
+                });
+            });
+        });
+    });
     
